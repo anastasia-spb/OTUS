ca9f6a6a76ae1671c537ef6ab64c5e5c605b219f
anastasia-spb <anastasia.vlasova6293@gmail.com>
2020-11-04 17:13:36 +0300

    Add basic custom allocator

--- allocators/src/my_allocator/my_allocator.h
+++ allocators/src/my_allocator/my_allocator.h
@@ -1,1 +1,53 @@
 #pragma once
+
+#include <iostream>
+#include <memory>
+
+namespace my_allocator
+{
+
+template <class T>
+struct MyAllocator
+{
+  using value_type = T;
+  MyAllocator();
+  template <class U> MyAllocator (const MyAllocator<U>&);
+  T* allocate(std::size_t n);
+  void deallocate(T* p, std::size_t n);
+};
+
+template <class T>
+MyAllocator<T>::MyAllocator() {}
+
+template <class T>
+template <class U>
+MyAllocator<T>::MyAllocator(const MyAllocator<U>& ) {}
+
+template <class T>
+T* MyAllocator<T>::allocate(std::size_t n)
+{
+	auto p = std::malloc(n * sizeof(T));
+	if (!p)
+		throw std::bad_alloc();
+	return reinterpret_cast<T *>(p);
+};
+
+template <class T>
+void MyAllocator<T>::deallocate(T* p, std::size_t )
+{
+	std::free(p);
+};
+
+template <class T, class U>
+constexpr bool operator== (const MyAllocator<T>&, const MyAllocator<U>&) noexcept
+{
+  return false;
+}
+
+template <class T, class U>
+constexpr bool operator!= (const MyAllocator<T>&, const MyAllocator<U>&) noexcept
+{
+  return false;
+}
+
+} // my_allocator
--- allocators/tests/my_allocator_tests/my_allocator_tests.cpp
+++ allocators/tests/my_allocator_tests/my_allocator_tests.cpp
@@ -1,2 +1,23 @@
 #include "my_allocator.h"
 #include <gtest/gtest.h>
+#include <map>
+
+TEST(MyAllocatorTest, ConstructMapUsingCustomAllocator)
+{
+    EXPECT_NO_THROW((std::map<int, int, std::less<int>, my_allocator::MyAllocator<std::pair<const int,int>>>{}));
+}
+
+TEST(MyAllocatorTest, AddElementToTheMapWithCustomAllocator)
+{
+    std::map<int, int, std::less<int>, my_allocator::MyAllocator<std::pair<const int,int>>> my_map{};
+    EXPECT_NO_THROW(my_map.insert(std::pair<int, int>(0, 0)));
+}
+
+TEST(MyAllocatorTest, AddTenElementsToTheMapWithCustomAllocator)
+{
+    std::map<int, int, std::less<int>, my_allocator::MyAllocator<std::pair<const int,int>>> my_map{};
+    for(auto i{0}; i < 10; ++i)
+    {
+        EXPECT_NO_THROW(my_map.insert(std::pair<int, int>(i, i)));
+    }
+}
