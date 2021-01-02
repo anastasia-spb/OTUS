1557e55af05bf762ccecee64d3ec9366ac2efd67
anastasia-spb <anastasia.vlasova6293@gmail.com>
2021-01-02 19:48:48 +0300

    Implement custom map allocator

--- allocators/src/my_allocator/my_allocator.h
+++ allocators/src/my_allocator/my_allocator.h
@@ -7,36 +7,91 @@ namespace my_allocator
 {
 
 template <class T>
-struct MyAllocator
+class MyAllocator
 {
-  using value_type = T;
-  MyAllocator();
-  template <class U> MyAllocator (const MyAllocator<U>&);
-  T* allocate(std::size_t n);
-  void deallocate(T* p, std::size_t n);
+  public:
+    using value_type = T;
+    MyAllocator(std::size_t n);
+    ~MyAllocator();
+    template <class U> MyAllocator (const MyAllocator<U>& other);
+    T* allocate(std::size_t n);
+    void deallocate(T* p, std::size_t n);
+    std::size_t GetReservedSize() const;
+
+  private:
+    std::size_t n_{0U};
+    std::size_t counter_{0U};
+    void* p_{nullptr};
+    T* current_pointer_{nullptr};
 };
 
+
+template <class T> 
+std::size_t MyAllocator<T>::GetReservedSize() const
+{
+  return n_;
+}
+
 template <class T>
-MyAllocator<T>::MyAllocator() {}
+MyAllocator<T>::~MyAllocator()
+{
+  current_pointer_ = nullptr;
+  if(!p_)
+  {
+    free(p_);
+  }
+}
+
+template <class T>
+MyAllocator<T>::MyAllocator(std::size_t n)
+:
+n_{n},
+counter_{0U}
+{
+  if(n_ > 0U)
+  {  
+    p_ = std::malloc(n_ * sizeof(T));
+  }
+}
 
 template <class T>
 template <class U>
-MyAllocator<T>::MyAllocator(const MyAllocator<U>& ) {}
+MyAllocator<T>::MyAllocator(const MyAllocator<U>& other)
+:
+n_(other.GetReservedSize()),
+counter_{0U}
+{
+  if(n_ > 0U)
+  {  
+    p_ = std::malloc(n_ * sizeof(T));
+  }
+}
 
 template <class T>
-T* MyAllocator<T>::allocate(std::size_t n)
+T* MyAllocator<T>::allocate(std::size_t)
 {
-	auto p = std::malloc(n * sizeof(T));
-	if (!p)
+	if (!p_ || (counter_ >= n_))
+  {
 		throw std::bad_alloc();
-	return reinterpret_cast<T *>(p);
+  }
+
+  if(counter_ == 0U)
+  {
+    current_pointer_ = reinterpret_cast<T *>(p_);
+  }
+  else
+  {
+    current_pointer_ += sizeof(T);
+  }
+
+  ++counter_; 
+
+	return current_pointer_;
 };
 
 template <class T>
-void MyAllocator<T>::deallocate(T* p, std::size_t )
-{
-	std::free(p);
-};
+void MyAllocator<T>::deallocate(T*, std::size_t)
+{};
 
 template <class T, class U>
 constexpr bool operator== (const MyAllocator<T>&, const MyAllocator<U>&) noexcept
--- allocators/tests/my_allocator_tests/my_allocator_tests.cpp
+++ allocators/tests/my_allocator_tests/my_allocator_tests.cpp
@@ -2,22 +2,63 @@
 #include <gtest/gtest.h>
 #include <map>
 
+using element_type = std::pair<const int, int>;
+using allocator_type = my_allocator::MyAllocator<element_type>;
+
+namespace
+{
+unsigned int factorial(unsigned int n)
+{
+    unsigned int result{1U};
+    for(auto i{1U}; i <= n; ++i)
+    {
+        result *= i;
+    }
+        
+    return result;
+}
+}
+
 TEST(MyAllocatorTest, ConstructMapUsingCustomAllocator)
 {
-    EXPECT_NO_THROW((std::map<int, int, std::less<int>, my_allocator::MyAllocator<std::pair<const int,int>>>{}));
+    EXPECT_NO_THROW((std::map<int, int, std::less<int>, allocator_type>{allocator_type(100U)}));
 }
 
 TEST(MyAllocatorTest, AddElementToTheMapWithCustomAllocator)
 {
-    std::map<int, int, std::less<int>, my_allocator::MyAllocator<std::pair<const int,int>>> my_map{};
-    EXPECT_NO_THROW(my_map.insert(std::pair<int, int>(0, 0)));
+    std::map<int, int, std::less<int>, allocator_type> my_map{allocator_type(1U)};
+    EXPECT_NO_THROW(my_map.emplace(std::pair<int, int>(0, 0)));
 }
 
 TEST(MyAllocatorTest, AddTenElementsToTheMapWithCustomAllocator)
 {
-    std::map<int, int, std::less<int>, my_allocator::MyAllocator<std::pair<const int,int>>> my_map{};
-    for(auto i{0}; i < 10; ++i)
+    std::map<int, int, std::less<int>, allocator_type> my_map{allocator_type(10U)};
+    for(auto i{0U}; i < 10U; ++i)
     {
-        EXPECT_NO_THROW(my_map.insert(std::pair<int, int>(i, i)));
+        EXPECT_NO_THROW(my_map.emplace(std::pair<int, int>(i, factorial(i))));
+    }
+}
+
+TEST(MyAllocatorTest, AddElementsToTheMapWithCustomAllocatorAndThenAccessThem)
+{
+    std::map<int, int, std::less<int>, allocator_type> my_map{allocator_type(10U)};
+    for(auto i{0U}; i < 10U; ++i)
+    {
+        EXPECT_NO_THROW(my_map.emplace(std::pair<int, int>(i, factorial(i))));
+    }
+
+    for(auto i{0U}; i < 10U; ++i)
+    {
+        EXPECT_NO_THROW(my_map[i]);
+        EXPECT_EQ(my_map[i], factorial(i));
+    }
+}
+
+TEST(MyAllocatorTest, AllocateZeroElementsTryAddElementToTheMapWithCustomAllocator_ExpectThrow)
+{
+    std::map<int, int, std::less<int>, allocator_type> my_map{allocator_type(0U)};
+    for(auto i{0U}; i < 1U; ++i)
+    {
+        EXPECT_THROW(my_map.emplace(std::pair<int, int>(i, factorial(i))), std::bad_alloc);
     }
 }
